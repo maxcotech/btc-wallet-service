95ab6cadb81d83f1437b09f1f576c34ab0520bf9
Chisom Maxwell <chiscomax2013@gmail.com>
2022-07-22 19:05:51 +0100

    added dependencies

--- src/services/AddressServices.ts
+++ src/services/AddressServices.ts
@@ -1,7 +1,16 @@
+import ECPairFactory from "ecpair";
 import Service from "./Service";
+import * as ecc from "tiny-secp256k1";
+import bitcoin from "bitcoinjs-lib";
 
 class AddressServices extends Service {
+    generateSegwitAddress(){
+        const ecpair = ECPairFactory(ecc);
+        const keypair = ecpair.makeRandom();
+        const {address} = bitcoin.payments.p2pkh({pubkey:keypair.publicKey, network: this.network});
+        
 
+    }
 }
 
 export default AddressServices
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,16 +2,18 @@ import bitcoin  from "bitcoinjs-lib";
 import {config} from "dotenv";
 
 class Service {
-
+    network: bitcoin.networks.Network;
+    baseUrl: string;
+    apiKey: string | undefined;
 
     constructor(){
-        const network = bitcoin.networks.testnet;
-        const baseUrl = "https://btc.getblock.io/testnet/";
-        const apiKey = process.env.SERVICE_API_KEY;
+        this.network = bitcoin.networks.testnet;
+        this.baseUrl = "https://btc.getblock.io/testnet/";
+        this.apiKey = process.env.SERVICE_API_KEY;
     }
 
     getConnection(){
-        
+        return this;
     }
 
 }
