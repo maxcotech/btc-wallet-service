a590d56017c0e6aa7648c277b863c100f906960e
Chisom Maxwell <chiscomax2013@gmail.com>
2023-03-01 09:30:19 +0100

    Gitignore Build , refactor address

--- src/controllers/AddressController.ts
+++ src/controllers/AddressController.ts
@@ -10,12 +10,16 @@ export default class AddressController extends Controller {
     public static async createAddress({req, res }: HttpRequestParams){
         const service = new AddressServices();
         const addressPayload = service.generateSegwitAddress();
+        return await this.saveAddress(addressPayload, req.body.userId);
+    }
+
+    public static async saveAddress(addressPayload: any, userId = "0"){
         if(addressPayload.address !== undefined){
             const addressModel = new Address();
             addressModel.address = addressPayload.address;
             addressModel.hash = addressPayload.hash ?? "";
             addressModel.pubKey = addressPayload.pubKey ?? "";
-            addressModel.userId = req.body.userId;
+            addressModel.userId = userId;
             addressModel.witness = addressPayload.output ?? "";
             addressModel.wifCrypt = addressPayload.wifCrypt;
             const addressRepo = AppDataSource.getRepository(Address);
