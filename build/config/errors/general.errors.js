15a869c65fa1947c73c850b7edf137aea8e37458
Chisom Maxwell <chiscomax2013@gmail.com>
2023-03-01 09:41:26 +0100

    Remove build from git track

--- build/config/errors/general.errors.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generalErrors = void 0;
-exports.generalErrors = {
-    internalServerError: "INTERNAL_SERVER_ERROR",
-    unknownError: "UNKNOWN_ERROR",
-    validationFailed: "VALIDATION_FAILED",
-    notAuthorized: "NOT_AUTHORIZED"
-};
